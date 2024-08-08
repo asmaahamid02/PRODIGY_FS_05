f8a06494537283204754d0eec359b6fe8ef814a6
Asmaa Hamid <asmaahamid002@gmail.com>
2024-08-08 14:57:49 +0300

    configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //all html files inside this folder and subfolders
+    './**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
