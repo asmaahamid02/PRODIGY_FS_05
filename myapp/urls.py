8d7eb1a3b7fb6b5800073cfc0216af623cbc7491
Asmaa Hamid <asmaahamid002@gmail.com>
2024-08-09 21:53:42 +0300

    added auth routes to the app routes

--- myapp/urls.py
+++ myapp/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls', namespace='post')),
+    path('auth/', include('account.urls', namespace='account')),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
