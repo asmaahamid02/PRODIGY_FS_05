932aed7cfc45496cafe5c6e7f878beca6223527b
Asmaa Hamid <asmaahamid002@gmail.com>
2024-08-14 20:40:29 +0300

    created a path for generating thumbnails

--- myapp/urls.py
+++ myapp/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import generate_thumbnail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls', namespace='post')),
     path('auth/', include('account.urls', namespace='account')),
+    path('thumbnail/<str:size>/<path:path>', generate_thumbnail, name='generate_thumbnail'),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ myapp/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpRequest, HttpResponse
+from PIL import Image
+from io import BytesIO
+from .utils import image_utils
+from urllib.parse import unquote
+import os
+from django.conf import settings
+
+def generate_thumbnail(request:HttpRequest, size: str, path:str) -> HttpResponse:
+    if not size or not path:
+        return HttpResponse(f"Both the image path and the size are required", status=400)
+    
+    path = unquote(path)
+    path = settings.PROJECT_DIR + path
+
+    try:
+        image = Image.open(path)
+    except Exception as e:
+        return HttpResponse(f"An error occured while opening the image: {e}", status=500)
+
+    sizes = size.split('x')
+
+    try:
+        width = int(sizes[0])
+        height = int(sizes[1]) if len(sizes) > 1 else None
+    except Exception as e:
+        return HttpResponse(f"An error occured while parsing the size: {e}", status=500)
+    
+    if height is None:
+        height = int(image.height * width / image.width)
+
+    image.thumbnail((width, height))
+
+    thumbnail_io = BytesIO()
+    image.save(thumbnail_io, format='webp', quality=90)
+    thumbnail_io.seek(0)
+
+    return HttpResponse(thumbnail_io, content_type=f"image/webp")
+    
